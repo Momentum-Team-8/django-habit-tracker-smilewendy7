f8f7dc56e5c9a553602d72a5397862d588152127
smilewendy7 <smilewendy7@gmail.com>
2021-07-07 14:03:11 -0400

    basic models set up nextsteps- fix issue in createDate

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, Habit, Record
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Habit)
+admin.site.register(Record)
--- core/models.py
+++ core/models.py
@@ -1,5 +1,31 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from datetime import date
 
 class User(AbstractUser):
-    pass
+    def __str__(self):
+        return self.username
+
+
+
+### Habit class
+class Habit(models.Model):
+    created_date = models.DateField(default=date.today)
+    name = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name="habits")
+    goal = models.IntegerField()
+    def __str__(self):
+        return self.name
+
+    
+
+
+class Record (models.Model):
+    date = models.DateField(null=True)
+    performance = models.IntegerField(null=True)
+    habit_name = models.ForeignKey(Habit, on_delete=models.CASCADE,
+                               related_name="records", null=True)
+
+    def __str__(self):
+        return self.created_date
